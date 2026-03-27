12e68507e8e560f85eb4756ab3929d800c5977b3
dhale-02 <dhale.caparros@caparrosui.com>
2026-03-28 00:46:28 +0800

    Update App.jsx

--- stayhealthy/src/App.jsx
+++ stayhealthy/src/App.jsx
@@ -2,16 +2,21 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar';
 import SignUp from './components/SignUp/SignUp';
 import Login from './components/Login/Login';
+import FindDoctorSearch from './components/FindDoctorSearch/FindDoctorSearch';
+import Notification from './components/Notification/Notification';
 import './App.css';
 
 function App() {
   return (
     <Router>
+      {/* Notification is placed here for application-wide visibility */}
+      <Notification />
       <Navbar />
       <Routes>
         <Route path="/" element={<h2 style={{textAlign:'center', marginTop:'100px'}}>Welcome to StayHealthy 🏥</h2>} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/appointments" element={<FindDoctorSearch />} />
       </Routes>
     </Router>
   );
